7fb46cecc6a3cfe4a5975210282fe9e0d67ded1f
Sai Kumar Munagala <talpasaikumar.munagala@gmail.com>
2022-07-22 10:52:20 +0530

    Added Users component and imported in App.js

--- react-phase1/src/App.js
+++ react-phase1/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Users from './components/Users';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+     { 
+     /* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>*/
+     }
+
+     <Users />
     </div>
   );
 }
